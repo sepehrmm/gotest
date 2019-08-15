05d36091ee5474f66be9f689d11e0893b11af277
Sepehr Sadatifar <135145+sepehrmm@users.noreply.github.com>
2019-08-15 16:51:05 +0700

    adf

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/sepehrmm/gotest
 
 go 1.12
 
-require github.com/sepehrmm/gotest1 v1.0.1
+require github.com/sepehrmm/gotest1/v2 v2.0.4
--- gotest.go
+++ gotest.go
@@ -1,7 +1,7 @@
 package gotest
 
 import (
-	"github.com/sepehrmm/gotest1"
+	"github.com/sepehrmm/gotest1/v2"
 )
 
 func Test(param1 string, param2 string) {
